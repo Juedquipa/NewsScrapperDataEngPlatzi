2178dcae029c74a88321f9eddc687bc8445a6ccf
Juedquipa <Juedquipa29@gmail.com>
2021-01-21 22:00:44 -0500

    Se arregló el .yaml y se agregó la funcionalidad de obtener links

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ import argparse
 import logging
 logging.basicConfig(level=logging.INFO)
 
+import news_page_objects as news
 from common import config
 
 
 logger = logging.getLogger(__name__)
 
 
-def _news_scraper(news_site):
-    host = config()['news_sites'][news_site]['url']
+def _news_scraper(news_site_uid):
+    host = config()['news_sites'][news_site_uid]['url']
 
     logging.info('Beginning scraper for {}'.format(host))
     logging.info('Finding links in homepage...')
+    homepage = news.HomePage(news_site_uid)
 
+    for link in homepage.article_links:
+        print(link)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -22,7 +26,8 @@ if __name__ == '__main__':
     parser.add_argument('news_site',
                         help='The news site that you want to scrape',
                         type=str,
-                        choices=news_site_choices)
+                        choices=news_site_choices
+                        )
 
     args = parser.parse_args()
     _news_scraper(args.news_site)
